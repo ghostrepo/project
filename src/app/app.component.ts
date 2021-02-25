03ff32e54b6f783da356d7623efa119050a293da
Johnson Noah (KCV5RKB) <KCV5RKB@UPS.COM>
2021-02-25 14:34:53 -0500

    create data processing button component create floor plan entry component update styles.css update app component update app module create navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eeProject';
+  title = 'VISUALIZATION OF ELECTROMAGNETIC RADIATION';
 }
