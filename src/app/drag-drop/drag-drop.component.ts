eac8dac0ce4140970eb08881fd53c82639ed2dfc
nj <nojoh5@morgan.edu>
2021-03-19 17:20:09 -0400

    update upload component and app component

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ApplianceTypes } from '../app-types';
+// import { Component, OnInit } from '@angular/core';
+// import { ApplianceTypes } from '../app-types';
+// import { ApplianceInterface } from '../applicance.interface';
 
-interface Appliances {
-    value: string;
-    viewValue: string;
-}
 
-@Component({
-    selector: 'drag-drop',
-    templateUrl: './drag-drop.component.html'
-})
+// @Component({
+//     selector: 'drag-drop',
+//     templateUrl: './drag-drop.component.html'
+// })
 
-export class DragDropComponent {
-    // constructor() {  }
-
-    selectedApp: string;
-
-      
-    appliances: Appliances[] = [
-        {value: 'micro-1', viewValue: 'Microwave'},
-        {value: 'fridge-2', viewValue: 'Refrigerator'},
-        {value: 'tv-3', viewValue: 'Television'},
-        {value: 'dry-4', viewValue: 'Drying Machine'},
-        {value: 'wash-5', viewValue: 'Washing Machine'},
-        {value: 'dish-6', viewValue: 'Dishwasher'},
-        {value: 'oven-7', viewValue: 'Oven'},
-        {value: 'tank-8', viewValue: 'Fish Tank'},
-        {value: 'sink-9', viewValue: 'Sink'},
-    ];
-}
+// export class DragDropComponent {
+//     selectedApp: string;
+// }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -29,4 +29,4 @@ export class UploadComponent implements OnInit {
   ngOnInit() {
   }
 
-}
+} 
