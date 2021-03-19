de90da12dd2a1b26abca8b9f9a64726b9bdea846
nj <nojoh5@morgan.edu>
2021-03-19 16:39:28 -0400

    add class for appliance emf values app-emf.ts

---
+++ src/app/app-emf.ts
@@ -0,0 +1,21 @@
+export class ApplianceEmf {  
+  public static microwave = '2.0 mG';
+
+  public static fridge = '3.5 mG';
+
+  public static tv = '4.0 mG';
+
+  public static dryer = '5.6 mG';
+
+  public static washer = '7.0 mG';
+
+  public static dish = '8.0 mG';
+
+  public static oven = '9.0 mG';
+
+  public static tank = '17.3 mG';
+
+  public static sink = '20 mG';
+
+
+}
